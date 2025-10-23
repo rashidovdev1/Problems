ab031cf65d65c16cac930f69ab169b7c363d9e92
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-23 17:05:11 +0500

    begin3

---
+++ cpp-problems/Qudrat/m4/begin3.cpp
@@ -0,0 +1,16 @@
+// S = a * b || P = 2 * (a + b)
+#include<iostream>
+using namespace std;
+
+int main(){
+ int a , b , S , P ;
+ cout<<"A ni kiriting: " ; cin>>a;
+ cout<<"B ni kiriting: " ; cin>>b;
+ 
+ S=a*b; 
+ P=2*(a+b) ;
+ 
+ cout<<"Yuzi="<<S<<endl;
+ cout<<"Perimetri="<<P<<endl;
+ return 0;
+}
