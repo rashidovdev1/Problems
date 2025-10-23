cfca1083a3ddf483f089917484b463dec9954381
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-23 17:07:27 +0500

    begin4

---
+++ cpp-problems/Qudrat/m4/begin4.cpp
@@ -0,0 +1,15 @@
+// L = pi * d || pi = 3.14
+#include<iostream>
+using namespace std;
+
+int main() {
+ float L, pi=3.14, d;
+
+ cout << "D ni kiriting:"; cin >> d;
+
+ L = pi * d;
+
+  cout << "Aylana uzunligi: " << L << endl;
+
+ return 0;
+}
