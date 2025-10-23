da18c12592879975ff694b46120f9be3ff6cecbf
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-23 17:02:39 +0500

    begin2

---
+++ cpp-problems/Qudrat/m4/begin2.cpp
@@ -0,0 +1,14 @@
+// kvadrat yuzi s = a*a
+#include <iostream>
+using namespace std;
+
+int main(){
+	int S, a;
+	
+	cout<<"A ni kiriting:" ; cin>>a;
+	S=a*a;
+	cout<<"Yuzi = "<<S<<endl;
+ 
+	return 0;
+	
+}
