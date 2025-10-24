c8a91a0ae4baacb025d85a39c73b2851656c47f2
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-24 09:48:33 +0500

    begin5

---
+++ cpp-problems/Qudrat/m4/begin5.cpp
@@ -0,0 +1,18 @@
+// 
+#include<iostream>
+using namespace std;
+
+int main() {
+ int V, a, S;
+
+ cout << "A ni kiriting:"; cin >> a;
+
+ V = a * a * a;
+ S = 6 * a * a;
+
+  cout << "Hajmi: " << V << endl;
+
+  cout << "To'la sirti: " << S << endl;
+
+ return 0;
+}
