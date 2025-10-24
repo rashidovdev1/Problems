ec13a79379f25142dccd9670fbe75f2b2a216006
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-24 10:07:05 +0500

    begin8

---
+++ cpp-problems/Qudrat/m4/begin8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float a, b, Arifmetik;
+	
+    cout << "a ni kiriting: "; cin >> a;
+    cout << "b ni kiriting: "; cin >> b;
+
+	Arifmetik = (a+b)/2;
+
+ 	cout << "Arifmetik: " << Arifmetik << endl;
+	return 0;
+}
