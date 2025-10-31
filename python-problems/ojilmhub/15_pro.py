34d930952ff25e32d0d35bc9ac127dd54466a4f0
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-10-31 18:47:05 +0500

    15_pro

---
+++ python-problems/ojilmhub/15_pro.py
@@ -0,0 +1,12 @@
+harf = input("Kichik harf kiriting: ")
+
+katta_harf = chr(ord(harf) - 32)
+print("Katta harf:", katta_harf)
+
+# ord() funksiyasi — belgining ASCII kodini (raqamga) qaytaradi.
+# ASCII jadvalida kichik va katta harflar orasidagi farq 32 ta
+# chr() funksiyasi raqamni belgiga aylantiradi.
+
+print("Easy broo!")
+# harf = input("Kichik harf kiriting: ")
+# print("Katta harf:", harf.upper())
