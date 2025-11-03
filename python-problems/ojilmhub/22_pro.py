520a312b80eba541ce250872bcb6e1caba6cd8d4
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-04 00:04:30 +0500

    22_pro

---
+++ python-problems/ojilmhub/22_pro.py
@@ -0,0 +1,8 @@
+# Nsonini o'qing. AgarNsoni 20 va 30 orasida (20 va 30 ham kiradi) bo'lsa,1ni chop eting. Aks holda0ni chop eting.
+
+ch = int(input("son kiriting: "))
+
+if (20 <= ch <= 30):
+    print(1)
+else:
+    print(0)
