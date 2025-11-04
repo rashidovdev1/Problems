aec0574f848bc5d92e909172f86494cd8ad9fb81
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-04 22:23:03 +0500

    begin6

---
+++ python-problems/Qudrat/m4/begin6.py
@@ -0,0 +1,10 @@
+# Paralelopiped tomoni a, b, c. V? S?
+
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+V =  a * b * c
+S = (a * b + b * c + a * c) * 2
+print(f"V = {V}")
+print(f"S = {S}")
