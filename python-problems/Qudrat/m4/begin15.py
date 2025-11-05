3135eae1300fabd9a07d2efec6b1901e8e4f6cd0
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-05 20:43:35 +0500

    begin15

---
+++ python-problems/Qudrat/m4/begin15.py
@@ -0,0 +1,9 @@
+# aylana yuzasi S bor.  diametr va radiusi aniqla
+
+import math
+S = float(input("Aylana yuzasi S: "))
+R = math.sqrt(S / math.pi)
+D = 2 * R
+
+print(f"R = {R}")
+print(f"D = {D}")
