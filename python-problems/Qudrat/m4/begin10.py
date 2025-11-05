256941427ea69221d5ff4da9f3ebb0af21615fc9
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-05 20:17:58 +0500

    begin10

---
+++ python-problems/Qudrat/m4/begin10.py
@@ -0,0 +1,13 @@
+# a va b, yigindi , kopaytma, kvadrat
+import math
+a = int(input('a = '))
+b = int(input('b = '))
+
+sum = a + b
+print(f'sum = {sum}')
+kop = a * b
+print(f'kop = {kop}')
+A = pow(a, 2)
+print(f'kv = {A}')
+B = pow(b, 2)
+print(f'kv = {B}')
