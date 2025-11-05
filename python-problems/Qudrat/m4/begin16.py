2e8cb3bb0a94a0a330a6871f5b34dace618bc415
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-05 20:57:40 +0500

    begin16

---
+++ python-problems/Qudrat/m4/begin16.py
@@ -0,0 +1,7 @@
+# son oqida 2ta nuqta orasidagi masofa aniqlansin
+
+x1 = float(input("Birinchi nuqta (x1): "))
+x2 = float(input("Ikkinchi nuqta (x2): "))
+
+masofa = abs(x2 - x1)
+print(f"Nuqtalar orasidagi masofa: {masofa}")
