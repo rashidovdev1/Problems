f20677783d75996718ee8e705bb1a8252fd44ebe
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-08 21:57:09 +0500

    38_pro2

--- python-problems/ojilmhub/38_pro.py
+++ python-problems/ojilmhub/38_pro.py
@@ -2,11 +2,17 @@
 _Pifagor_sonlari deb2ta sonning kvadratlari yig'indisi3-sonkvadratiga teng bo'lgan sonlarga aytiladi.a,b va c
 Pifagor sonlari bo'lishi uchun ular quyidagi shartlardan birini qoniqtirishi kerak"""
 
+
+
+
+def is_pifagorean(a,b,c):
+    if (a * a + b * b == c * c) or (a * a + c * c == b * b) or (b * b + c * c == a * a):
+       return True
+    else:
+        return False
+
 a = int(input('a = '))
 b = int(input('b = '))
 c = int(input('c = '))
 
-if (a*a + b*b == c*c) or (a*a + c*c == b*b) or (b*b + c*c == a*a):
-    print('True')
-else:
-    print('False')
+print(is_pifagorean(a,b,c))
