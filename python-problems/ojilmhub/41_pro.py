1ce55ed87202bdd05c4ab3f134131439468b390b
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-09 11:10:04 +0500

    41_pro-keyingi-tub-son

--- python-problems/ojilmhub/41_pro.py
+++ python-problems/ojilmhub/41_pro.py
@@ -2,14 +2,23 @@
 agarNsoni tub bo'lsa o'zini chop eting
 agarNtub bo'lmasa, undan keyingi tub sonni chop eting"""
 
-#N sonidan 1ta oldingi tub son
-n = int(input("N gacha tub sonlar: "))
-x = 0
-for i in range(2, n):
+#N sonidan keyingi 1-tub son
+n = int(input("N ni kiriting: "))
+
+i = n + 1
+while True:
     tub = True
-    for j in range(2, int(i**0.5)+1):
-        if i % j == 0:
-            tub = False
+    if i >= 2:
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                tub = False
+                break
+    else:
+        tub = False
+
     if tub:
-        x = i
-print(x)
+        print(i)
+        break
+    i += 1
+
+
