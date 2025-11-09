4f17328055099e5493ed677cd63a578d1cdc576f
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-09 10:55:45 +0500

    41_pro-oldingi-tub-son_fix

--- python-problems/ojilmhub/41_pro.py
+++ python-problems/ojilmhub/41_pro.py
@@ -5,13 +5,11 @@ agarNtub bo'lmasa, undan keyingi tub sonni chop eting"""
 #N sonidan 1ta oldingi tub son
 n = int(input("N gacha tub sonlar: "))
 x = 0
-for i in range(2, n + 1):
+for i in range(2, n):
     tub = True
     for j in range(2, int(i**0.5)+1):
         if i % j == 0:
             tub = False
-
     if tub:
         x = i
-
-print(x, end=" ")
+print(x)
