1034c92f5851ccc5d455006bf49d9846e0d7435b
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-09 11:34:37 +0500

    41_pro

--- python-problems/ojilmhub/41_pro.py
+++ python-problems/ojilmhub/41_pro.py
@@ -2,23 +2,37 @@
 agarNsoni tub bo'lsa o'zini chop eting
 agarNtub bo'lmasa, undan keyingi tub sonni chop eting"""
 
-#N sonidan keyingi 1-tub son
 n = int(input("N ni kiriting: "))
 
-i = n + 1
-while True:
-    tub = True
-    if i >= 2:
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                tub = False
-                break
-    else:
-        tub = False
+tub = True
+if n < 2:
+    tub = False
+else:
+    for j in range(2, int(n**0.5)+1):
+        if n % j == 0:
+            tub = False
+            break
 
-    if tub:
-        print(i)
-        break
-    i += 1
+if tub is True:
+    print(n)
+else:
+    i = n + 1
+    while True:
+        tub = True
+        if i < 2:
+            tub = False
+        else:
+            for j in range(2, int(i**0.5)+1):
+                if i % j == 0:
+                    tub = False
+                    break
+        if tub:
+            print(i)
+            break
+        i += 1
+
+
+
+
 
 
