bd4250ead58b8ac93cf72053026728abebb80e87
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-19 23:35:41 +0500

    begin17

---
+++ python-problems/Qudrat/m4/begin17.py
@@ -0,0 +1,13 @@
+
+A = int(input("Birinchi nuqta (A): "))
+B = int(input("Ikkinchi nuqta (B): "))
+C = int(input('Uchinchi nuqta (C): '))
+
+AC = abs(A - C)
+BC = abs(B - C)
+yigindi = AC + BC
+
+print("AC =", AC)
+print("BC =", BC)
+print("AC + BC =", yigindi)
+
