dd60c1be23e23ccb4ecd80ef4dc5a119057582e3
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-19 23:41:11 +0500

    begin18

---
+++ python-problems/Qudrat/m4/begin18.py
@@ -0,0 +1,11 @@
+A = int(input("Birinchi nuqta (A): "))
+B = int(input("Ikkinchi nuqta (B): "))
+C = int(input('Uchinchi nuqta (C): '))
+
+AC = abs(A - C)
+BC = abs(B - C)
+kopaytma = AC * BC
+
+print("AC =", AC)
+print("BC =", BC)
+print("AC × BC =", kopaytma)
